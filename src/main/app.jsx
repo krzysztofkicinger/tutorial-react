b09e497bf1f06b542bda1d5318eefab63cc98479
krzysztofkicinger <krzysztofkicinger@gmail.com>
2017-11-23 11:18:15 +0100

    [Part1] Rendering simple react component

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,1 +1,9 @@
-document.getElementById("container").innerText = "This is from webpack";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = <h1>Hello from React!</h1>;
+
+ReactDOM.render(
+    element,
+    document.getElementById("container")
+);
